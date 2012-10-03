b22ded4af16ece6d83b6b5de1d5695498a559b16
Przemysław Janowski <przewski@gmail.com>
2012-10-03 23:30:25 +0200

    Turn method changed. Added distance between wheels variable.

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,7 +2,7 @@ public class MainClass
 {
    public static void main(String[] args) throws InterruptedException
    {      
-      new Movement(4.1).start();
+      new Movement(4.1, 10.5).start();
       //TODO Appropriate power value to the battery level 
    }
 }
